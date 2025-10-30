ddb8faea6d6a303fadd03ed81285acf7ebd84f84
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-10-30 08:43:23 -0400

    feat: remove unused/outdated hooks causing enable errors

--- kolibri-kolibri_usb_backup_plugin-plugin/kolibri_kolibri_usb_backup_plugin_plugin/kolibri_plugin.py
+++ kolibri-kolibri_usb_backup_plugin-plugin/kolibri_kolibri_usb_backup_plugin_plugin/kolibri_plugin.py
@@ -1,21 +1,21 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 
-from kolibri.core.webpack import hooks as webpack_hooks
+# from kolibri.core.webpack import hooks as webpack_hooks
 
-from kolibri.plugins.base import KolibriPluginBase
+from kolibri.plugins import KolibriPluginBase
 
-from kolibri.plugins.user import hooks
+# from kolibri.plugins.user import hooks
 
 
 class KolibriUsbBackupPluginPlugin(KolibriPluginBase):
     pass
 
 
-class KolibriUsbBackupPluginPluginAsset(webpack_hooks.WebpackBundleHook):
-    unique_slug = "kolibri_kolibri_usb_backup_plugin_plugin_module"
-    src_file = "assets/src/module.js"
+# class KolibriUsbBackupPluginPluginAsset(webpack_hooks.WebpackBundleHook):
+#     unique_slug = "kolibri_kolibri_usb_backup_plugin_plugin_module"
+#     src_file = "assets/src/module.js"
 
 
-class KolibriUsbBackupPluginPluginInclusionHook(hooks.UserSyncHook):
-    bundle_class = KolibriUsbBackupPluginPluginAsset
+# class KolibriUsbBackupPluginPluginInclusionHook(hooks.UserSyncHook):
+#     bundle_class = KolibriUsbBackupPluginPluginAsset
