061800345baef6d6f46ee66b0193a7f48cd65fa4
Annika Pomeroy <annikapomeroy@gmail.com>
2026-01-17 21:07:08 -0500

    Create index2.vue

---
+++ ui-files/index2.vue
@@ -0,0 +1,10 @@
+# Changes to kolibri/plugins/facility/assets/src/views/DataPage/index.vue
+
+# Add  <BackupInterface /> under 
+# <ImportInterface v-if="canUploadDownloadFiles" />
+# <SyncInterface />
+# (Line 135)
+
+# Add import BackupInterface from './BackupInterface'; to line 214
+
+# Add BackupInterface, to line 230
