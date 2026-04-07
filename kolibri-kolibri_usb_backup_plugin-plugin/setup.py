fd9bbabcad97f785e7009383a1b7cf239579bf09
AnnikaPomeroy <annikapomeroy@gmail.com>
2026-04-06 22:10:37 -0400

    Version 0.1.3

--- kolibri-kolibri_usb_backup_plugin-plugin/setup.py
+++ kolibri-kolibri_usb_backup_plugin-plugin/setup.py
@@ -4,11 +4,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from setuptools import find_packages, setup
 
-name = "kolibri_kolibri_usb_backup_plugin_plugin"
+name = "kolibri-kolibri-usb-backup-plugin-plugin"
 
 setup(
     name=name,
-    version="0.1.2",
+    version="0.1.3",
     description="Kolibri USB Backup Plugin",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
