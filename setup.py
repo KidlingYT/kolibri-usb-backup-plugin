48efeb1898697fbef2f7364ad4a943b9b893f65d
AnnikaPomeroy <annikapomeroy@gmail.com>
2026-04-06 20:36:02 -0400

    Build file updates

--- setup.py
+++ setup.py
@@ -9,15 +9,14 @@ name = "kolibri_kolibri_usb_backup_plugin_plugin"
 
 setup(
     name=name,
-    version="0.1.0",
+    version="0.1.2",
     description="Kolibri USB Backup Plugin",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="Learning Equality",
     author_email="info@learningequality.org",
     url="https://github.com/KidlingYT/kolibri-usb-backup-plugin",
-    packages=find_packages(),
-    package_dir={name: name},
+    packages=find_packages(include=[name, f"{name}.*"]),
     include_package_data=True,
     license="MIT",
     zip_safe=False,
